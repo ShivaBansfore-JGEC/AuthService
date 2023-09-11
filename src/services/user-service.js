91364a852a76a294360c21f4d2295e74083e407d
ShivaBansfore-JGEC <shivabansfore123@gmail.com>
2023-09-11 08:38:42 +0530

    feat: create token and verify token jwt functionality added

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,5 +1,6 @@
+const jwt = require('jsonwebtoken');
 const UserRepository = require('../repositories/user-repository');
-
+const { JWT_KEY } = require('../config/serverConfig');
 class UserService {
     constructor(){
         this.userRepository = new UserRepository();
@@ -11,6 +12,26 @@ class UserService {
             return user;
         }catch(error){
             console.log('something went wrong int the service layer');
+            throw error;
+        }
+    }
+
+    createToken(user){
+        try{
+            const result = jwt.sign(user, JWT_KEY, {expiresIn: '1d'});
+            return result;
+        }catch(error){
+            console.log('something went wrong in the service layer!');
+            throw error;
+        }
+    }
+
+    verifyToken(token){
+        try{
+            const result = jwt.verify(token, JWT_KEY);
+            return result;
+        }catch(error){
+            console.log('something went wrong in verifying token!', error);
             throw error;
         }
     }
